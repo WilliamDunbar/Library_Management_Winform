413f1791543b095926ac054435b216b52c98104d
William <williamdunbar2000@gmail.com>
2021-04-30 14:43:59 +0700

    Giao dien lam bang winform 30/04/2021

---
+++ QLTVWF/QuanLyDauSach.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLTVWF
+{
+     public partial class QuanLyDauSach : Form
+     {
+          public QuanLyDauSach()
+          {
+               InitializeComponent();
+          }
+
+          private void AdvanceButton_Click(object sender, EventArgs e)
+          {
+               //ForgroundPanel.Controls.Clear();
+               Form quanlycuonsach = new QuanLyCuonSach();
+               quanlycuonsach.TopLevel = false;
+               quanlycuonsach.Dock = DockStyle.Fill;
+               Backround_Panel.Controls.Add(quanlycuonsach);
+               Backround_Panel.Tag = quanlycuonsach;
+               quanlycuonsach.BringToFront();
+               quanlycuonsach.Show();
+          }
+
+          private void guna2GradientPanel5_Paint(object sender, PaintEventArgs e)
+          {
+
+          }
+     }
+}
